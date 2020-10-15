10023045569cf94af4eb4b28fab69de9fe151ae6
Autumn Pham <autumnpham@Autumns-MacBook.lan>
2020-10-15 13:50:02 -0700

    controllers and models

---
+++ controllers/reviews.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: application/json');
+include_once __DIR__ . '/../models/review.php';
+
+if($_REQUEST['action'] === 'index'){
+    echo json_encode(Reviews::all());
+} else if ($_REQUEST['action'] === 'create'){
+  $request_body = file_get_contents('php://input');
+  $body_object = json_decode($request_body);
+  $new_person = new Review(null, $body_object->name, $body_object->rating, $body_object->description);
+  $all_reviews = Reviews::create($new_review);
+  echo json_encode($all_reviews);
+} else if ($_REQUEST['action'] === 'update'){
+    $request_body = file_get_contents('php://input');
+    $body_object = json_decode($request_body);
+    $updated_review = new Review($_REQUEST['id'], $body_object->name, $body_object->rating, $body_object->description);
+    $all_reviews = Reviews::update($updated_review);
+    echo json_encode($all_reviews);
+} else if ($_REQUEST['action'] === 'delete'){
+    $all_reviews = Reviews::delete($_REQUEST['id']);
+    echo json_encode($all_reviews);
+}
+?>
---
+++ models/review.php
@@ -0,0 +1,74 @@
+<?php
+
+$dbconn = null;
+if(getenv('DATABASE_URL')){
+	$connectionConfig = parse_url(getenv('DATABASE_URL'));
+	$host = $connectionConfig['host'];
+	$user = $connectionConfig['user'];
+	$password = $connectionConfig['pass'];
+	$port = $connectionConfig['port'];
+	$dbname = trim($connectionConfig['path'],'/');
+	$dbconn = pg_connect(
+		"host=".$host." ".
+		"user=".$user." ".
+		"password=".$password." ".
+		"port=".$port." ".
+		"dbname=".$dbname
+	);
+} else {
+	$dbconn = pg_connect("host=localhost dbname=reviews");
+}
+
+class Review {
+    public $id;
+    public $name;
+    public $rating;
+    public $description;
+    public function __construct($id, $name, $rating, $description) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->rating = $rating;
+        $this->description = $description;
+    }
+}
+
+class Reviews {
+  static function create($review){
+    $query = "INSERT INTO review (name, rating, description) VALUES ($1, $2, $3)";
+    $query_params = array($review->name, $review->rating, $review->description);
+    pg_query_params($query, $query_params);
+    return self::all();
+  }
+  static function update($updated_review){
+    $query = "UPDATE review SET name = $1, rating = $2, description = $3 WHERE id = $3";
+    $query_params = array($updated_review->name, $updated_review->rating, $updated_review->description, $updated_review->id);
+    pg_query_params($query, $query_params);
+    return self::all();
+  }
+  static function delete($id){
+    $query = "DELETE FROM review WHERE id = $1";
+    $query_params = array($id);
+    pg_query_params($query, $query_params);
+    return self::all();
+  }
+    static function all(){
+        $reviews = array();
+        $results = pg_query("SELECT * FROM reviews ORDER BY id ASC");
+        $row_object = pg_fetch_object($results);
+        while($row_object !== false){
+
+            $new_review = new Review(
+                intval($row_object->id),
+                $row_object->name,
+                intval($row_object->rating),
+                $row_object->description
+            );
+            $reviews[] = $new_review;
+            $row_object = pg_fetch_object($results);
+        }
+        return $reviews;
+    }
+}
+
+
+?>
