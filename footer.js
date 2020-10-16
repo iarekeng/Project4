949549b4a076a5bdc24388b744bf638907fa7294
Autumn Pham <autumnpham@Autumns-MacBook.lan>
2020-10-16 13:32:29 -0700

    footer

--- footer.js
+++ footer.js
@@ -1,21 +1,22 @@
 class Footer extends React.Component {
   render = () => {
     return (
-      <div className="footer">
+      <footer>
         <div className="footer1">
-          <ul className="footer-links">
-            <li>ABOUT</li>
-            <li>RESERVATIONS</li>
-            <li>GIFT CARDS</li>
-            <li>CONTACT US</li>
-          </ul>
+            <p>ABOUT</p>
+            <p>RESERVATIONS</p>
+            <p>GIFT CARDS</p>
+            <p>CONTACT US</p>
         </div>
         <div className="footer2">
         <p>SOME GRAPHIC HERE</p>
         </div>
         <div className="footer3">
+          <a href="#" target="_blank"><i className="fab fa-instagram-square fa-3x fa-fw"></i></a>
+          <a href="#"><i className="fab fa-facebook-square fa-3x fa-fw"></i></a>
+          <a href="#" target="_blank"><i className="fab fa-twitter-square fa-3x fa-fw"></i></a>
         </div>
-      </div>
+      </footer>
     )
   }
 }
