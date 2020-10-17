34ecb0156481b4bdbe3cfec8084a03bcdc6db8dd
Autumn Pham <autumnpham@Autumns-MacBook.lan>
2020-10-17 11:24:10 -0700

    added carousel.js

---
+++ carousel.js
@@ -0,0 +1,48 @@
+class Carousel extends React.Component {
+  render = () => {
+    return (
+      <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="https://image.zmenu.com/large/938470/20141104192622727000.jpg" className="d-block w-100" alt="...">
+            <div className="carousel-caption d-none d-md-block">
+               <h5>Crème Brûlée French Toast</h5>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src="https://www.seriouseats.com/images/2012/07/20120725-lobster-ceviche-latin-cuisine-6.jpg" className="d-block w-100" alt="...">
+            <div className="carousel-caption d-none d-md-block">
+               <h5>Lobster Ceviche</h5>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src="https://oursaltykitchen.com/wp-content/uploads/2019/01/whole30-steak-bites-11.jpg" className="d-block w-100" alt="...">
+            <div className="carousel-caption d-none d-md-block">
+               <h5>Rib Eye</h5>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src="https://thefirstyearblog.com/wp-content/uploads/2017/12/Chocolate-Peanut-Butter-Mousse-9-600x899.jpg" className="d-block w-100" alt="...">
+            <div className="carousel-caption d-none d-md-block">
+               <h5>Reese's Gelato and Mousse</h5>
+            </div>
+          </div>
+        </div>
+        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </a>
+    </div>
+
+    )
+  }
+}
