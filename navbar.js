3565ab34667d51d8360fd29793f708045b85977f
Autumn Pham <autumnpham@Autumns-MacBook.lan>
2020-10-18 10:52:22 -0700

    flexed reviews

--- navbar.js
+++ navbar.js
@@ -2,7 +2,7 @@ class Nav extends React.Component {
   render = () => {
     return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">LOGO OR GRAPHIC HERE?</a>
+  <a className="navbar-brand" href="#">Pinch of Salt</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
